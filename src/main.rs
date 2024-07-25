f9786ba710c773f6ff902d83cd3aceaed2a06cd6
BIGGASSS <timcooksgrandpa@gmail.com>
2024-07-25 16:31:26 +0800

    added auto-close feature

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let mut input1 = String::new();
@@ -15,4 +17,6 @@ fn main() {
     let input1: i32 = input1.trim().parse().expect("Please type a number!");
     let input2: i32 = input2.trim().parse().expect("Please type a number!");
     println!("The result is: {}", input1 + &input2);
+    println!("The program will be closed in 5 seconds.");
+    thread::sleep(Duration::from_secs(5));
 }

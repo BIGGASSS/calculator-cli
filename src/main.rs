361a53f2169b77fdd5f889a763a9f07e87071c2e
BIGGASSS <timcooksgrandpa@gmail.com>
2024-07-25 15:44:37 +0800

    implementation of addition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+    println!("Calculator!! (only addition is supported right now)");
+    println!("Enter your first number: ");
+    io::stdin()
+        .read_line(&mut input1)
+        .expect("Failed to read line");
+    println!("Enter your second number: ");
+    io::stdin()
+        .read_line(&mut input2)
+        .expect("Failed to read line");
+    let input1: i32 = input1.trim().parse().expect("Please type a number!");
+    let input2: i32 = input2.trim().parse().expect("Please type a number!");
+    println!("The result is: {}", input1 + &input2);
 }

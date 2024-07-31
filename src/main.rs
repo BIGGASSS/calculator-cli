2b50bf29b4cbab8283194f35ff00a6f1572f0093
BIGGASSS <timcooksgrandpa@gmail.com>
2024-07-31 09:44:02 +0800

    fixed syntax

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let mut calMode = String::new();
+    let mut cal_mode = String::new();
     let mut input1 = String::new();
     let mut input2 = String::new();
     println!("Calculator!! (only intergers are supported right now)");
     println!("Choose your mode: ");
     println!("1. Addition, 2. Subtraction, 3. Multiplication, 4. Division");
     io::stdin()
-        .read_line(&mut calMode)
+        .read_line(&mut cal_mode)
         .expect("Failed to read line");
-    let calMode: i32 = calMode.trim().parse().expect("Please type a number!");
-    if calMode == 1 { // Addition
+    let cal_mode: i32 = cal_mode.trim().parse().expect("Please type a number!");
+    if cal_mode == 1 { // Addition
         println!("Enter your first number: ");
         io::stdin()
             .read_line(&mut input1)
@@ -25,7 +25,7 @@ fn main() {
         let input1: i32 = input1.trim().parse().expect("Please type a number!");
         let input2: i32 = input2.trim().parse().expect("Please type a number!");
         println!("The result is: {}", input1 + &input2);
-    } else if calMode == 2 { // Subtraction
+    } else if cal_mode == 2 { // Subtraction
         println!("Enter your first number: ");
         io::stdin()
             .read_line(&mut input1)
@@ -37,7 +37,7 @@ fn main() {
         let input1: i32 = input1.trim().parse().expect("Please type a number!");
         let input2: i32 = input2.trim().parse().expect("Please type a number!");
         println!("The result is: {}", input1 - &input2);
-    } else if calMode == 3 { // Multiplication
+    } else if cal_mode == 3 { // Multiplication
         println!("Enter your first number: ");
         io::stdin()
             .read_line(&mut input1)
@@ -49,7 +49,7 @@ fn main() {
         let input1: i32 = input1.trim().parse().expect("Please type a number!");
         let input2: i32 = input2.trim().parse().expect("Please type a number!");
         println!("The result is: {}", input1 * &input2);
-    } else if calMode == 4 { // Division
+    } else if cal_mode == 4 { // Division
         println!("Enter your first number: ");
         io::stdin()
             .read_line(&mut input1)
